dee3d863c72269df302b166c8a0ca1e015973104
Andres <65410285+AndresGB1@users.noreply.github.com>
2022-03-11 23:29:20 -0500

    Update 2_verificacion.sql

--- 2_verificacion.sql
+++ 2_verificacion.sql
@@ -1,14 +1,15 @@
 create function verificar() returns trigger AS $before_insert_orden$
 	begin 
-		if exists (select cp.id_carro from carrocompra as cp where cp.id_carro = id_compra) then
+		if exists (select cp.id_carro from carrocompra as cp where cp.id_carro = new.id_compra) then
 			raise exception 'paila';
 			rollback;
 		else
+			insert into compra values (new.id_compra,new.direccionenvio,new.tipo,new.metodo,new.estado);
 			raise 'inserción satisfactoria';
 		end if;
 	end
 $before_insert_orden$ language plpgsql;
 
-
-create trigger before_insert_orden before update on compra
+create trigger before_insert_orden before insert on compra
 	for each row execute procedure verificar();
+
