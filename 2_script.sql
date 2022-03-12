1e0c8c0113f82b5629348e66e8b734e774dc370f
Andres <65410285+AndresGB1@users.noreply.github.com>
2022-03-11 23:30:50 -0500

    Create 2_script.sql

---
+++ 2_script.sql
@@ -0,0 +1,8 @@
+insert into usuario values ('Andresito','Andres','Galvis',10023949,'Galvisito@hot.com',1,'Hola','Calle','Bogota');
+insert into comprador values ('Andresito','Hola soy andresito');
+insert into carroCompra values (0,'Andresito','PCs','Asus','2','D','20/07/2003');
+insert into tipoenvio values('avion');
+insert into compra values (0,'Cra 23 no x2','avion','Tar credito','U');
+delete from compra * where id_compra = 0;
+select * from carrocompra;
+SELECT * from compra;
